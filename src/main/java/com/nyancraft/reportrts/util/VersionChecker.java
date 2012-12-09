3c5ff9a3df9b40a5ebe86d32cc5c6996abad0425
ProjectInfinity <a.sjur91@gmail.com>
2012-12-09 16:36:27 +0100

    VersionChecker should be optional. Although I would prefer if people used it.

--- src/main/java/com/nyancraft/reportrts/util/VersionChecker.java
+++ src/main/java/com/nyancraft/reportrts/util/VersionChecker.java
@@ -14,6 +14,7 @@ import java.net.URLConnection;
 public class VersionChecker {
 
     public boolean upToDate(){
+        if(!ReportRTS.getPlugin().getConfig().getBoolean("versionCheck")) return true;
         try {
             final URLConnection connection = new URL("http://regularbox.com/api/check.php").openConnection();
             connection.setConnectTimeout(3000);
@@ -25,7 +26,7 @@ public class VersionChecker {
                 ReportRTS.getPlugin().versionString = version;
                 if (!ReportRTS.getPlugin().getDescription().getVersion().equals(version)) {
                     ReportRTS.getPlugin().getLogger().info("Found a different version available: " + version);
-                    ReportRTS.getPlugin().getLogger().info("Check http://dev.bukkit.org/server-mods/reportrts/");
+                    ReportRTS.getPlugin().getLogger().info("Check http://dev.bukkit.org/server-mods/reportrts/ for a new version.");
                     return false;
                 }
                 return true;
