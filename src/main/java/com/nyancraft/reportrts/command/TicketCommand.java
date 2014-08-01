a5bb661d3a4058386b8250d95d29a6ead6ba2d64
ProjectInfinity <a.sjur91@gmail.com>
2014-08-01 15:41:39 +0200

    Cleanup, final testing may begin.

--- src/main/java/com/nyancraft/reportrts/command/TicketCommand.java
+++ src/main/java/com/nyancraft/reportrts/command/TicketCommand.java
@@ -17,7 +17,7 @@ public class TicketCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        /** TODO: Argument checker, DO NOT LEAVE THIS UNCOMMENTED IN PRODUCTION **/
+        /** Argument checker, DO NOT LEAVE THIS UNCOMMENTED IN PRODUCTION *
         int i = -1;
         for(String arg : args) {
             i++;
